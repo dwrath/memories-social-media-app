fad9cf29b45710145edc853baa642ea6eea39aeb
Damian Wrather <damianwrather@Damians-Mini.home>
2023-01-08 12:36:44 -0600

    edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-
-import PostDetails from './components/PostDetails/PostDetails';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Auth from './components/Auth/Auth';
-import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import PostDetails from "./components/PostDetails/PostDetails";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import CreatorOrTag from "./components/CreatorOrTag/CreatorOrTag";
+import dotenv from "dotenv";
+dotenv.config();
 
 const App = () => {
-  const user = JSON.parse(localStorage.getItem('profile'));
+  const user = JSON.parse(localStorage.getItem("profile"));
 
   return (
     <BrowserRouter>
@@ -20,7 +22,7 @@ const App = () => {
           <Route path="/posts" exact component={Home} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" exact component={PostDetails} />
-          <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
+          <Route path={["/creators/:name", "/tags/:name"]} component={CreatorOrTag} />
           <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
         </Switch>
       </Container>
--- server/index.js
+++ server/index.js
@@ -16,10 +16,14 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("Memories API");
+});
+
 const PORT = process.env.PORT || 8080;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
